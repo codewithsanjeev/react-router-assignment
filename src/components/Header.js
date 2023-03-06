73f6dde4d3d9ca7189be5bb2394d2128a0f5eeea
codewithsanjeev <codewithsanjeev@gmail.com>
2023-03-06 10:20:05 +0530

    Add Links via Link

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 function Header() {
     return (
@@ -10,9 +11,9 @@ function Header() {
                 <section>
                     <nav className='site-width mx-auto'>
                         <ul>
-                            <li><a href="/">Home</a></li>
-                            <li><a href="/about">About</a></li>
-                            <li><a href="/contact">Contact</a></li>
+                            <Link to="/"><li>Home</li></Link>
+                            <Link to="/about"> <li>About</li></Link>
+                            <Link to="/contact"><li>Contact</li></Link>
                         </ul>
                     </nav>
                 </section>
