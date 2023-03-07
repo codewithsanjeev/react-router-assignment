32ed587097450c8ba08550b063551dbea6a02d40
codewithsanjeev <codewithsanjeev@gmail.com>
2023-03-07 12:17:44 +0530

    To Link page in react with react-router - always use Link to not a

--- src/components/Body.js
+++ src/components/Body.js
@@ -8,7 +8,7 @@ function Body() {
                     <section className='home-text'>
                         <h2>AI is the next Big Things</h2>
                         <p> Lorem ipsum dolor sit, amet consectetur adipisicing elit. Incidunt voluptas voluptates aliquid, optio sapiente repudiandae omnis repellendus odio deserunt? Quis incidunt, explicabo ullam adipisci quasi cumque voluptas perferendis suscipit, quia, dolorem tempora reiciendis nostrum minima repudiandae? Obcaecati, consequatur iure. Vero, dicta asperiores ea placeat odit est optio distinctio aspernatur maiores! </p>
-                        <a href="/contact">Contact Us</a>
+                        <Link to="/contact">Contact Us</Link>
                     </section>
 
                     <section>Something</section>
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 function Header() {
     return (
@@ -9,16 +10,12 @@ function Header() {
                 </section>
                 <section>
                     <nav className='site-width mx-auto'>
-                        {/* <ul>
+                        <ul>
                             <Link to="/"><li>Home</li></Link>
                             <Link to="/about"> <li>About</li></Link>
                             <Link to="/contact"><li>Contact</li></Link>
-                        </ul> */}
-                        <ul>
-                            <li> <a href="/">Home</a></li>
-                            <li><a href="/about">About</a></li>
-                            <li><a href="/contact">Contact</a></li>
                         </ul>
+
                     </nav>
                 </section>
             </header>
